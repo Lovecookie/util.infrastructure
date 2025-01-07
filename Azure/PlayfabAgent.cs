a85dfa86eca02ae154f02482d1c9d9f6d53aa951
smbaek86 <smbaek@nerdfor.com>
2025-01-07 15:32:04 +0900

    애져 빌드 보기 추가

--- Azure/PlayfabAgent.cs
+++ Azure/PlayfabAgent.cs
@@ -1,18 +1,18 @@
-﻿
-
+﻿using PlayFab.AuthenticationModels;
 using PlayFab.MultiplayerModels;
+
 
 namespace Util.Infrastructure.Azure;
 
 public class PlayfabAgent
-{   
+{
     private readonly IConfiguration _configuration;
     private readonly ILogger<PlayfabAgent> _logger;
     private string _titleID = "";
     private string _secretKey = "";
     private string _buildID = "";
     private string _entityToken = "";
-    
+
 
     public PlayfabAgent(IConfiguration configuration, ILogger<PlayfabAgent> logger)
     {
@@ -27,17 +27,17 @@ public class PlayfabAgent
         PlayFabSettings.staticSettings.DeveloperSecretKey = _secretKey;
     }
 
-    public async Task<TOutcome<string>> TryGetEntityToken()
+    public async Task<TOutcome<string>> TryGetEntityTokenAsync()
     {
-        if(_entityToken != "")
+        if (_entityToken != "")
         {
             return TOutcome.Ok(_entityToken);
         }
 
-        var req = new PlayFab.AuthenticationModels.GetEntityTokenRequest();
+        var req = new GetEntityTokenRequest();
 
         var res = await PlayFabAuthenticationAPI.GetEntityTokenAsync(req);
-        if(res.Error != null)
+        if (res.Error != null)
         {
             _logger.LogError("Error getting entity token: {0}", res.Error.ErrorMessage);
             return TOutcome.Err<string>(res.Error.ErrorMessage);
@@ -48,12 +48,12 @@ public class PlayfabAgent
         return TOutcome.Ok(_entityToken);
     }
 
-    public async Task<TOutcome<RequestMultiplayerServerResponse>> TryGetMultiplayerServer(string sessionID, string accountID)
+    public async Task<TOutcome<RequestMultiplayerServerResponse>> TryGetMultiplayerServerAsync(string sessionID, string accountID)
     {
-        var req = new PlayFab.MultiplayerModels.RequestMultiplayerServerRequest();
+        var req = new RequestMultiplayerServerRequest();
         req.BuildId = _buildID;
 
-        var regionsOutcome = await TryGetRegions();
+        var regionsOutcome = await TryGetRegionsAsync();
         if (false == regionsOutcome.Success)
         {
             return TOutcome.Err<RequestMultiplayerServerResponse>(regionsOutcome.Message);
@@ -76,18 +76,18 @@ public class PlayfabAgent
         return TOutcome.Ok(res.Result);
     }
 
-    public async Task<TOutcome<IEnumerable<string>>> TryGetRegions()
+    public async Task<TOutcome<IEnumerable<string>>> TryGetRegionsAsync()
     {
-        var req = new PlayFab.MultiplayerModels.GetBuildRequest();
+        var req = new GetBuildRequest();
         req.BuildId = _buildID;
 
         var res = await PlayFabMultiplayerAPI.GetBuildAsync(req);
-        if(res.Error != null)
+        if (res.Error != null)
         {
             return TOutcome.Err<IEnumerable<string>>(res.Error.ErrorMessage);
         }
 
-        var region = res.Result.RegionConfigurations.Select( x => x.Region);
+        var region = res.Result.RegionConfigurations.Select(x => x.Region);
         if (region.Count() == 0)
         {
             return TOutcome.Err<IEnumerable<string>>("No region found");
@@ -95,4 +95,57 @@ public class PlayfabAgent
 
         return TOutcome.Ok(region);
     }
+
+    public async Task<TOutcome<ListVirtualMachineSummariesResponse>> TryGetVMSummariesAsync()
+    {
+        var req = new ListVirtualMachineSummariesRequest();
+        var regionsOutcome = await TryGetRegionsAsync();
+        if (false == regionsOutcome.Success)
+        {
+            return TOutcome.Err<ListVirtualMachineSummariesResponse>(regionsOutcome.Message);
+        }
+
+        req.Region = regionsOutcome.Value!.First();
+        req.BuildId = _buildID;
+        var res = await PlayFabMultiplayerAPI.ListVirtualMachineSummariesAsync(req);
+        if (res.Error != null)
+        {
+            return TOutcome.Err<ListVirtualMachineSummariesResponse>(res.Error.ErrorMessage);
+        }
+
+        return TOutcome.Ok(res.Result);
+    }
+
+    public async Task<TOutcome<ListMultiplayerServersResponse>> TryGetMultiplayerServersAsync()
+    {
+        var req = new ListMultiplayerServersRequest();
+        var regionsOutcome = await TryGetRegionsAsync();
+        if (false == regionsOutcome.Success)
+        {
+            return TOutcome.Err<ListMultiplayerServersResponse>(regionsOutcome.Message);
+        }
+
+        req.Region = regionsOutcome.Value!.First();
+        req.BuildId = _buildID;
+        var res = await PlayFabMultiplayerAPI.ListMultiplayerServersAsync(req);
+        if (res.Error != null)
+        {
+            return TOutcome.Err<ListMultiplayerServersResponse>(res.Error.ErrorMessage);
+        }
+
+        return TOutcome.Ok(res.Result);
+    }
+
+    public async Task<TOutcome<ListBuildSummariesResponse>> TryGetBuildSummariesAsync()
+    {
+        var req = new ListBuildSummariesRequest();
+        var res = await PlayFabMultiplayerAPI.ListBuildSummariesV2Async(req);
+        if (res.Error != null)
+        {
+            return TOutcome.Err<ListBuildSummariesResponse>(res.Error.ErrorMessage);
+        }
+
+        return TOutcome.Ok(res.Result);
+    }
+
 }
